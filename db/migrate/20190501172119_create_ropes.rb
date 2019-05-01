2f7b36e777475c20672bc3a349cda877f19df83b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-01 17:26:18 +0000

    creating migrations

--- db/migrate/20190501172119_create_ropes.rb
+++ db/migrate/20190501172119_create_ropes.rb
@@ -1,4 +1,10 @@
 class CreateRopes < ActiveRecord::Migration
   def change
+    create_table :ropes do |t|
+      t.string :primary_color
+      t.string :pcord_color
+      t.integer :total_climbs
+      t.integer :element_id
+    end
   end
 end
--- db/migrate/20190501172124_create_inspections.rb
+++ db/migrate/20190501172124_create_inspections.rb
@@ -1,4 +1,9 @@
 class CreateInspections < ActiveRecord::Migration
   def change
+    t.date :climb_date
+    t.string :comments
+    t.integer :user_id
+    t.integer :element_id
+    t.timestamps
   end
 end
