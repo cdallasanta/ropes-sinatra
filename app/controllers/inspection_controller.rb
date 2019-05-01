f4d266701214ce6dd5cb1d4503f083ce012d64ae
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-01 21:01:31 +0000

    new inspection page

--- app/controllers/inspection_controller.rb
+++ app/controllers/inspection_controller.rb
@@ -2,9 +2,18 @@ class InspectionController < ApplicationController
   get '/inspections/new' do
     if logged_in?
       @elements = Element.all
-      erb :'/inspections/new'
+      erb :'/inspections/new_select_element'
     else
       redirect '/login'
+    end
+  end
+
+  get '/inspections/new/:element_slug' do
+    if logged_in?
+      @element = Element.find_by_slug(params[:element_slug])
+      erb :'/inspections/new'
+    else
+      redirect 'login'
     end
   end
 
--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,12 @@
 class Element < ActiveRecord::Base
   has_many :ropes
   has_many :inspections
+
+  def slug
+    return self.name.downcase.gsub(' ','-').gsub(/[^\w-]/,'')
+  end
+
+  def self.find_by_slug(text)
+    self.all.detect {|instance| instance.slug == text}
+  end
 end
